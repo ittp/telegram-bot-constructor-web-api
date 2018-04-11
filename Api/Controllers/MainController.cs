4a1f36def2e107298d0daf4647aa43511174ea8b
Mikhail Carlin <m.karlin@dodopizza.com>
2018-04-11 22:40:06 +0300

    bot page

--- Api/Controllers/MainController.cs
+++ Api/Controllers/MainController.cs
@@ -1,38 +1,74 @@
-﻿using Api.Repositories;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Api.Models;
+using Api.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace Api.Controllers
 {
-	public class MainController : Controller
+	public partial class MainController : Controller
 	{
 		private readonly IConfiguration _configuration;
+		private readonly UsersRepository _usersRepository;
+		private readonly BotsRepository _botsRepository;
+		private readonly TextMessageAnswersRepository _textMessageAnswersRepository;
+		private readonly InterviewsRepository _interviewsRepository;
+		private readonly InterviewAnswersRepository _interviewAnswersRepository;
+		private readonly InlineUrlKeysRepository _inlineUrlKeysRepository;
+		private readonly InlineKeysRepository _inlineKeysRepository;
+		private readonly HttpClient _httpClient;
 
-		public MainController(IConfiguration configuration, UsersRepository usersRepository,
+		public MainController(IConfiguration configuration, UsersRepository usersRepository, BotsRepository botsRepository,
 			TextMessageAnswersRepository textMessageAnswersRepository, InterviewsRepository interviewsRepository,
 			InterviewAnswersRepository interviewAnswersRepository, InlineUrlKeysRepository inlineUrlKeysRepository,
 			InlineKeysRepository inlineKeysRepository)
 		{
 			_configuration = configuration;
+			_usersRepository = usersRepository;
+			_botsRepository = botsRepository;
+			_textMessageAnswersRepository = textMessageAnswersRepository;
+			_interviewsRepository = interviewsRepository;
+			_interviewAnswersRepository = interviewAnswersRepository;
+			_inlineUrlKeysRepository = inlineUrlKeysRepository;
+			_inlineKeysRepository = inlineKeysRepository;
+			_httpClient = new HttpClient();
 		}
 
 		public IActionResult Index()
 		{
-			return View();
+			var bots = _botsRepository.GetBots();
+
+
+			return View(bots);
 		}
 
-		public IActionResult About()
-		{
-			ViewData["Message"] = "Your application description page.";
 
-			return View();
+		public IActionResult TextMessages()
+		{
+			var bots = _botsRepository.GetBots();
+
+
+			return View(bots);
 		}
 
-		public IActionResult Contact()
+		[Route("/bot")]
+		public async Task<IActionResult> Bot(string id)
 		{
-			ViewData["Message"] = "Your contact page.";
+			var bot = _botsRepository.GetBot(id);
 
-			return View();
+			var result = await _httpClient.GetStringAsync($"{_configuration["RunnerApiUrl"]}/check?id={id}");
+			var parsedResult = JsonConvert.DeserializeObject<Response>(result);
+			var startMessage = _botsRepository.GetStartMessage(id);
+
+			return View(new BotViewModel
+			{
+				Bot =  bot,
+				Status = Convert.ToBoolean(parsedResult.status),
+				StartMessage = startMessage
+			});
 		}
 	}
 }
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -52,7 +52,7 @@ namespace Api
 			{
 				routes.MapRoute(
 					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
+					template: "{controller=Main}/{action=Index}/{id?}");
 			});
 		}
 	}
