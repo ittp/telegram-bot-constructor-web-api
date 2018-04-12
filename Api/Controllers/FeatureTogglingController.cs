e20fdd6a37a9293612790845aededffc8983e303
Mikhail Carlin <m.karlin@dodopizza.com>
2018-04-12 03:10:46 +0300

    feature toggling

---
+++ Api/Controllers/FeatureTogglingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Api.Models;
+using Api.Repositories;
+using Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace Api.Controllers
+{
+	public class FeatureTogglingController : Controller
+	{
+		private readonly BotsRepository _botsRepository;
+
+		public FeatureTogglingController(BotsRepository botsRepository)
+		{
+			_botsRepository = botsRepository;
+		}
+
+
+		[Route("/networking")]
+		public async Task<RedirectResult> SetNetWorking(string id, bool status)
+		{
+			_botsRepository.SetNetWorkingStatus(id, status);
+
+			return Redirect($"/bot?id={id}");
+		}
+
+		[Route("/congnitiveservices")]
+		public async Task<RedirectResult> SetCongnitiveServices(string id, bool status)
+		{
+			_botsRepository.SetCognitiveServicesStatus(id, status);
+
+			return Redirect($"/bot?id={id}");
+		}
+
+	}
+}
